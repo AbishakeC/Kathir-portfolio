fb26c6064ac334d4be054c8002054b7caa2f55d8
Abishake <abishakec17a@gmail.com>
2025-08-09 01:56:40 +0530

    front

--- backend/model.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb://localhost:27017/curdoperations';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
